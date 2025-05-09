20aa4298af7f9cbb02cbdf27a6bd38cffc8b7d1e
SHEN LEE <138705412+shenleeosialai@users.noreply.github.com>
2025-05-09 12:10:44 +0300

    custom backend auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -73,6 +73,14 @@ LOGOUT = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 
