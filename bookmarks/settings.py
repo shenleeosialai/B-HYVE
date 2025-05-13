a68751f1634673bfccf92511ad84ab7175bb9f7f
SHEN LEE <138705412+shenleeosialai@users.noreply.github.com>
2025-05-13 14:48:50 +0300

    added google as a sign in option

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -81,6 +81,23 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '834014884051-5ete3phku05310rpan9f6uutgaj0dbes.apps.googleusercontent.com' # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
 
 WSGI_APPLICATION = 'bookmarks.wsgi.application'
