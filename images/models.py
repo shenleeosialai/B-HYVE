36f87a1ce1def8618e5238591667f05c0c55c4e2
shen lee <shenleekhalid@gmail.com>
2025-06-10 18:23:36 +0300

    signals for denormalizing

--- images/models.py
+++ images/models.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
 
@@ -7,26 +7,26 @@ from django.urls import reverse
 class Image(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name='images_created',
+        related_name="images_created",
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
     description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateField(auto_now_add=True)
     users_like = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name='images_liked',
-        blank=True,
+        settings.AUTH_USER_MODEL, related_name="images_liked", blank=True
     )
+    total_likes = models.PositiveIntegerField(default=0)
 
     class Meta:
         indexes = [
-            models.Index(fields=['-created']),
+            models.Index(fields=["-created"]),
+            models.Index(fields=["-total_likes"]),
         ]
-        ordering = ['-created']
+        ordering = ["-created"]
 
     def __str__(self):
         return self.title
@@ -37,4 +37,4 @@ class Image(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('images:detail', args=[self.id, self.slug])
+        return reverse("images:detail", args=[self.id, self.slug])
