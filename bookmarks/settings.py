3aa86eedd397387072da46e9ea85623ff10674ba
shen lee <shenleekhalid@gmail.com>
2025-06-11 23:16:11 +0300

    debug toolbar

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
     'easy_thumbnails',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+internal_ips = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'bookmarks.urls'
