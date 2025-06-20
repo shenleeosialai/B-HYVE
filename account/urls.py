f03e62784019dcf24b08d672caea72099c499bbd
shen lee <shenleekhalid@gmail.com>
2025-06-20 18:41:55 +0300

    stlying profile

--- account/urls.py
+++ account/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('my-profile/', views.my_profile, name='my_profile'),
     path('users/', views.user_list, name='user_list'),
     path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
