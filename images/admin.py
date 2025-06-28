b8d31d2910c5432413815bf0c701104a000ec2ca
shen lee <shenleekhalid@gmail.com>
2025-06-28 08:28:28 +0300

    successful uploads for video

--- images/admin.py
+++ images/admin.py
@@ -5,5 +5,5 @@ from .models import Image
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'image', 'created']
-    list_filter = ['created']
+    list_display = ['title', 'slug', 'image', 'created', 'autoplay']
+    list_filter = ['autoplay', 'created']
